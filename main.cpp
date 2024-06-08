de16fe64ee145a228201698f1111019391fb5495
evanh1122 <evanhan1122@gmail.com>
2024-06-08 15:57:32 -0400

    finished up genArray function to generate a line and split it among the processors

--- main.cpp
+++ main.cpp
@@ -3,41 +3,58 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <map>
 
-bool generate_points(int *array, int num_points, int range_start, int range_end, int seed) {
-    bool did_work = false;
+using namespace std;
 
-    if (num_points <= 0) {
-        return did_work;
-    }
 
+// creates an array filled with random numbers
+void genArray(int size, int range_start, int range_end, int seed) {
+    // pair<location on the x-axis, value>
+    map<int, int> line;
+    int nProcs, iProc;
+    MPI_Comm_rank(MPI_COMM_WORLD, &iProc);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
+
+    // calculates what processor takes care of how much of the array
+    int subSize = size / nProcs;
+    if (size % nProcs != 0) ++subSize;
+
+    // set the seed for the random number generator
     srand(seed);
-    array[0] = num_points;
-    for (int i = 1; i < num_points + 1; i++) {
-        array[i] = rand() % (range_end - range_start + 1) + range_start;
-        did_work = true;
+
+    // fills the line with random numbers
+    // each processor only fills their part of the line
+    // rand() % (upper - lower + 1) + lower
+    for (int i = iProc * subSize; i < i + subSize && i < size; ++i) {
+        line[i] = rand() % (range_end - range_start + 1) + range_start;
     }
-    return did_work;
 }
 
-bool divide_list(int *array, int nProcs, int *owner_array) {
-    bool did_work = false;
+// Broadcast the location of one line (smaller line) (MPI_broadcast). 
+// Then, create an array that is the same size. Array is filled with -1 on both processors. 
+// Go through locations and ask, is this point on my processor? 
+// If the point is on the processor, then fill in the rank of the processor onto the array. 
+// The indices of the array are associated with the location of the line. 
+// Do MPI_Allgather, take the max value, now every processor has a full array filled with 0, 1, 2, or 3, and there are no -1 left. 
+// Once you have the locations worked out, put a function on a processor that knows all of the data and try and get that data to all of the other processors
 
-    int array_size = array[0];
-    int sub_size = array_size / nProcs;
-    owner_array[0] = array_size;
 
-    for (int i = 0; i < nProcs; i++) {
-        int j = 0;
-        while (j < sub_size) {
-            owner_array[i * sub_size + j + 1] = i;
-            j++;
-        }
-    }
+// I NEED TO FIGURE OUT HOW TO STORE THE LINE. RIGHT NOW, THE MAP GETS DESTROYED AFTER genArray FINISHES
+int main() {
+    int nProcs, iProc;
+    bool did_work;
 
-    return did_work;
-}
+    // SET THE SIZE OF THE LINE
+    // LINE WILL GO FROM x = 0 TO x = size - 1
+    int size = 10;
 
-main() {
-    
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &iProc);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
+
+    MPI_Finalize();
+    return 0;
 }
