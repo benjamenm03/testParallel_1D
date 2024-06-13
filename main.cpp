a530fea9566d65fa34cc5a0021fab2c96e22219f
benjamenm03 <benjamen@umich.edu>
2024-06-13 18:08:10 -0400

    FIX: gen_temp_ref function not working properly. FIXED!

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ std::map<double, double> gen_temp_ref(int iProc, int nProcs, std::map<double, do
     int total_length = grid_ref.size(); // Total number of points in temp_ref
     double start_index = grid_ref.begin()->first; // Start index of grid_ref
     double end_index = grid_ref.rbegin()->first; // End index of grid_ref
-    int sub_size = total_length; // Number of points in each submap (processor num_points)
+    int sub_size = total_length / nProcs; // Number of points in each submap (processor num_points)
     int local_start = iProc * sub_size; // Local map start index
     double local_end = local_start + sub_size - 1; // Local map end index
     double step_size = (end_index - start_index + 1) / total_length; // Step size for each index
@@ -59,12 +59,10 @@ std::map<double, double> gen_temp_ref(int iProc, int nProcs, std::map<double, do
     for (double i = start_index; i <= end_index; i += step_size) {
         temp_ref[i] = -1;
         local_ownership_map[i] = -1;
-    }
-
-    // Generate processor's chunk of temp_ref
-    for (double i = local_start; i <= local_end; i += step_size) {
-        temp_ref[i] = 200 + 100 * sin(grid_ref[i] * 2 * M_PI / 100);
-        local_ownership_map[i] = iProc;
+        if (i >= local_start && i <= local_end) {
+            temp_ref[i] = 200 + 100 * sin(grid_ref[i] * 2 * M_PI / 100);
+            local_ownership_map[i] = iProc;
+        }
     }
 
     // Return did_work as function output boolean
@@ -74,7 +72,7 @@ std::map<double, double> gen_temp_ref(int iProc, int nProcs, std::map<double, do
 // PACK_MAP:
 // Packs a map of doubles into a vector of doubles to be used in MPI handling
 std::vector<double> pack_map(std::map<double, double> &map) {
-    std::vector<double> packed_map(map.size() * 2);
+    std::vector<double> packed_map;
     for (auto const &pair : map) {
         packed_map.push_back(pair.first);
         packed_map.push_back(pair.second);
@@ -92,6 +90,8 @@ std::map<double, double> unpack_vector(std::vector<double> &packed_map) {
     return map;
 }
 
+// PRINT_DATA (map<double, double>):
+// Prints out a map of doubles with a header
 void print_data(int iProc, std::map<double, double> &data, std::string header) {
     if (iProc == 0) {
         std::cout << "\n" << header << std::endl;
@@ -101,6 +101,8 @@ void print_data(int iProc, std::map<double, double> &data, std::string header) {
     }
 }
 
+// PRINT_DATA (vector<double>):
+// Prints out a vector of doubles with a header
 void print_data(int iProc, std::vector<double> &data, std::string header) {
     if (iProc == 0) {
         std::cout << "\n" << header << std::endl;
@@ -152,8 +154,10 @@ int main(int argc, char **argv) {
     MPI_Allreduce(&packed_grid_copy_ownership[0], &packed_global_grid_copy_ownership[0], packed_grid_copy_ownership.size(), MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     std::map<double, double> global_grid_copy_ownership = unpack_vector(packed_global_grid_copy_ownership); // Unpack packed_global_grid_copy_ownership back into a map of doubles
 
+    // Print statements to read out the following data (print_data works with vector<double> and map<double, double>):
     print_data(iProc, global_grid_copy, "Global Grid Copy:");
     print_data(iProc, global_grid_copy_ownership, "Global Grid Copy Ownership:");
+    print_data(iProc, temp_ref, "Local Temp Ref:");
 
     MPI_Finalize(); // Finalize MPI
 }
